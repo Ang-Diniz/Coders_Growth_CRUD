6e0036f737e22ef31b9d6c8d307207fb5eaca5cf
Ang-Diniz <andregs.diniz@gmail.com>
2023-04-13 14:09:55 -0300

    Terminei as telas, criei a classe Pessoa e adicionei os atributos, e fiz a conexão entre a tela inicial e a tela de cadastro

--- src/GerenciamentodeClientes/Pessoa.cs
+++ src/GerenciamentodeClientes/Pessoa.cs
@@ -1,12 +1,4 @@
-﻿using GerenciamentodeClientes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
-
-namespace GerenciamentodeClientes
+﻿namespace GerenciamentodeClientes
 {
     public class Pessoa
     { 
