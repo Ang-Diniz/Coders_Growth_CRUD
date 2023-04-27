748959f8dbdf4fa1955c26dce5f406c99483e49d
Ang-Diniz <andregs.diniz@gmail.com>
2023-04-27 08:51:57 -0300

    criar códigos de migraçãO para criar tabela

--- src/GerenciamentodeClientes/Program.cs
+++ src/GerenciamentodeClientes/Program.cs
@@ -1,15 +1,58 @@
+using System;
+using System.Linq;
+
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+
+using Microsoft.Extensions.DependencyInjection;
+
 namespace GerenciamentodeClientes
 {
-    internal static class Program
+    class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                // Put the database update into a scope to ensure
+                // that all resources will be disposed.
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static ServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer2016()
+                    // Set the connection string
+                    .WithGlobalConnectionString("server=INVENT089;database=GerenciamentoDeClientes;Persist Security Info=True;User ID=sa;Password=sap@123")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddLogTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new TelaInicial());
         }
