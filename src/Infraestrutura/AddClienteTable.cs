169635dbabe1a3d249b10968179a729677937a40
Ang-Diniz <andregs.diniz@gmail.com>
2023-05-12 13:23:55 -0300

    Identity

--- src/Infraestrutura/AddClienteTable.cs
+++ src/Infraestrutura/AddClienteTable.cs
@@ -9,7 +9,7 @@ namespace Infraestrutura
         public override void Up()
         {
             Create.Table("Cliente")
-                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity(1, 1)
                 .WithColumn("Nome").AsString().NotNullable()
                 .WithColumn("CPF").AsString().NotNullable()
                 .WithColumn("Email").AsString().NotNullable()
