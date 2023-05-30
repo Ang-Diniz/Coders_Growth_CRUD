dbef3e4f6ffd0471df1d656528d56beeb8a20b59
Ang-Diniz <andregs.diniz@gmail.com>
2023-05-30 09:03:53 -0300

    .

--- src/ClientesAPI/wwwroot/controller/App.controller.js
+++ src/ClientesAPI/wwwroot/controller/App.controller.js
@@ -1,10 +1,19 @@
 ﻿sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.cliente.controller.App", {
         onInit: function () {
 
+            const Tabela = 'https://localhost:7147/api/cliente';
+
+            fetch(Tabela).then(res => res.json()).then(res => {
+                const dataModel = new JSONModel();
+                dataModel.setData({
+                    items: res
+                });
+                this.getView().setModel(dataModel, "cliente")
+            })
         }
     });
 });
